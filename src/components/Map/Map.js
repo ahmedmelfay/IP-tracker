9110f0c85681851dd18274ac1fc555513389a9ac
ahmedmelfay <ahmedmelfay@gmail.com>
2021-09-19 13:03:48 +0200

    Configured leaflet map to display correct location based on IP Address

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,27 @@
 import React from "react";
 import mapStyles from "../../styles/map/map.module.scss";
 import { Popup, MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useSelector } from "react-redux";
 const Map = () => {
-  return (
+  const state = useSelector((state) => state);
+  console.log(state);
+  return state.loading ? (
+    <h1>Loading..</h1>
+  ) : (
     <section id={mapStyles.map}>
       <div style={{ position: "relative", zIndex: 0 }}>
-        <MapContainer className={mapStyles.mapContainer} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer
+          className={mapStyles.mapContainer}
+          center={[state.data.location.lat, state.data.location.lng]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>Your Location</Popup>
+          <Marker position={[state.data.location.lat, state.data.location.lng]}>
+            <Popup>IP Location</Popup>
           </Marker>
         </MapContainer>
       </div>
